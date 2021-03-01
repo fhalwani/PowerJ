2abd8bd324809c2af8368773fa9cf328e3d755f7
Fawaz Halwani <fhalwani@gmail.com>
2021-02-28 19:26:57 -0500

    Delete ca/eorla/fhalwani/powerj directory

--- ca/eorla/fhalwani/powerj/DataMasterOrder.java
+++
@@ -1,6 +0,0 @@
-package ca.eorla.fhalwani.powerj;
-
-class DataMasterOrder {
-	short groupID = 0;
-	String name = "";
-}
--- ca/eorla/fhalwani/powerj/DataSpecimenOrder.java
+++
@@ -1,7 +0,0 @@
-package ca.eorla.fhalwani.powerj;
-
-class DataSpecimenOrder {
-	short groupID = 0;
-	short qty = 0;
-	String name = "";
-}
